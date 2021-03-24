cc97ab1d8c4eebb3e3b45ed2fbedc5648f27330d
marceloapuppio <marcelopuppio@gmail.com>
2021-03-24 20:50:00 -0300

    css

---
+++ my-portfolio/src/components/main.js/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import React, { Fragment } from "react";
+
+export default function Main() {
+  return (
+    <Fragment>
+      <main className="main">
+        <section className="section section-home"></section>
+        <section className="section section-about"></section>
+        <section className="section section-works"></section>
+        <div></div>
+      </main>
+    </Fragment>
+  );
+}
--- my-portfolio/src/pages/page-ppal.js
+++ my-portfolio/src/pages/page-ppal.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "../components/aside-header";
 import AsideHeader from "../components/aside-header";
+import Main from "../components/main.js";
+import "./index.css";
 
 export default function PagePpal() {
-  return <AsideHeader />;
+  return (
+    <Fragment>
+      <AsideHeader />
+      <Main></Main>
+    </Fragment>
+  );
 }
